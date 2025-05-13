f9630fd2d12ff6408b82684ccb86ad7ab0fa8162
Steviamp <amplianitistavr@outlook.com>
2025-05-13 13:41:47 +0300

    .

--- NQWSZUS/front/data/stores.js
+++ NQWSZUS/front/data/stores.js
@@ -1,22 +1,14 @@
-﻿axports const stores = [
+﻿export const stores = [
     {
         id: 1,
         name: "Store A",
         host: "127.0.0.1",
-        port: 8899,
-        services: [
-            { id: 1, name: "Service Type 1" },
-            { id: 2, name: "Service Type 2" }
-        ]
+        port: 8899
     },
     {
         id: 2,
         name: "Store B",
         host: "127.0.0.1",
-        port: 8899,
-        services: [
-            { id: 1, name: "Service Type 1" },
-            { id: 3, name: "Service Type 3" }
-        ]
+        port: 8899
     }
 ];
