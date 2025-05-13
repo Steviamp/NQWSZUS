c0c5845c8ae55642764324bbcabdb24486d1c590
Steviamp <amplianitistavr@outlook.com>
2025-05-13 14:52:01 +0300

    .

---
+++ NQWSZUS/Controllers/ServiceTypeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NQWSZUS.Services;
+using ServiceReference;
+
+namespace NQWSZUS.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ServiceTypeController : ControllerBase
+    {
+        private readonly ISoapService _soapService;
+        public ServiceTypeController(ISoapService soapService)
+        {
+            _soapService = soapService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<ServiceType>>> GetAll(
+        [FromQuery] string host,
+        [FromQuery] int port)
+        {
+            var list = await _soapService.GetServiceTypeListAsync(host, port);
+            return Ok(list);
+        }
+    }
+}
+
--- NQWSZUS/Controllers/ServiceTypeStatusController.cs
+++ NQWSZUS/Controllers/ServiceTypeStatusController.cs
@@ -1,56 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
 using NQWSZUS.Services;
 
 namespace NQWSZUS.Controllers
 {
-    using Microsoft.AspNetCore.Mvc;
-    using ServiceReference;
-    using System.Threading.Tasks;
-
-    namespace NQWSReception.Controllers
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ServiceTypeStatusController : ControllerBase
     {
-        [ApiController]
-        [Route("api/[controller]")]
-        public class ServiceTypeStatusController : ControllerBase
+        private readonly ISoapService _soapService;
+        public ServiceTypeStatusController(ISoapService soapService)
         {
-            private readonly ISoapService _soapService;
-            public ServiceTypeStatusController(ISoapService soapService)
-            {
-                _soapService = soapService;
-            }
+            _soapService = soapService;
+        }
 
-            [HttpGet]
-            public async Task<ActionResult<List<ServiceType>>> GetAll(
+        [HttpGet("{serviceType}")]
+        public async Task<ActionResult<bool>> GetStatus(
+            int serviceType,
             [FromQuery] string host,
-            [FromQuery] int port)
-            {
-                var list = await _soapService.GetServiceTypeListAsync(host, port);
-                return Ok(list);
-            }
+            [FromQuery] int port
+        )
+        {
+            var isActive = await _soapService.IsServiceTypeStatusActiveAsync(serviceType, host, port);
+            return Ok(isActive);
+        }
 
-            [HttpGet("{serviceType}")]
-            public async Task<ActionResult<bool>> GetStatus(
-                int serviceType,
-                [FromQuery] string host,
-                [FromQuery] int port
-            )
-            {
-                var isActive = await _soapService.IsServiceTypeStatusActiveAsync(serviceType, host, port);
-               return Ok(isActive);
-            }
-
-            [HttpPost("{serviceType}")]
-            public async Task<IActionResult> SetStatus(
-                int serviceType,
-                [FromQuery] bool status,
-                [FromQuery] string host,
-                [FromQuery] int port
-            )
-            {
-                var success = await _soapService.ActivateServiceTypeAsync(serviceType, status, host, port);
-                if (!success)
-                    return StatusCode(500, "Failed to toggle service type status.");
-                return Ok(new { serviceType, status });
-            }
+        [HttpPost("{serviceType}")]
+        public async Task<IActionResult> SetStatus(
+            int serviceType,
+            [FromQuery] bool status,
+            [FromQuery] string host,
+            [FromQuery] int port
+        )
+        {
+            var success = await _soapService.ActivateServiceTypeAsync(serviceType, status, host, port);
+            if (!success)
+                return StatusCode(500, "Failed to toggle service type status.");
+            return Ok(new { serviceType, status });
         }
     }
 }
